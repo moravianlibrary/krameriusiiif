21298f974e7cb935cba39515b6e0ce673a72b250
mtravnicek <75383.muni@gmail.com>
2020-09-24 15:45:29 +0000

    Updated docker config

--- docker-s2i/build.sh
+++ docker-s2i/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ ! -f ./s2i ]; then
+    echo "s2i not found - downloading..."
+    sh ./get_s2i.sh
+fi
 docker build -t krameriusiiif-builder .
-s2i build https://github.com/MartinRumanek/krameriusiiif.git krameriusiiif-builder martinrumanek/krameriusiiif
+./s2i build https://github.com/MartinRumanek/krameriusiiif.git krameriusiiif-builder martinrumanek/krameriusiiif
